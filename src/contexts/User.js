9d1ad53df9e69eb8b1aa0ec12ca4cfae10278614
ur2ku <lim8667@gmail.com>
2022-05-02 12:03:12 +0900

    [] Context Modify

--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,4 +1,18 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
-const UserContext = createContext({name: 'Im KunYoung'});
+const UserContext = createContext({
+    user: {name: ''},
+    dispatch:()=>{},
+});
+
+
+const UserProvider = ({children}) => {
+    const [name, setName] = useState("Im KunYoung");
+    const value = {user: {name}, dispatch: setName};
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+const UserConsumer = UserContext.Consumer;
+
+export {UserProvider, UserConsumer};
 export default UserContext;
